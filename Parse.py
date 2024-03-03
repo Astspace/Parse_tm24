e50bd5c6fac98203a4bc0e27c5f2a429ac1a7da0
Astspace <asteroid.space@gmail.com>
2024-03-03 22:41:33 +0300

    Creating excel files for categories_2

--- Parse.py
+++ Parse.py
@@ -7,16 +7,16 @@ import os
 
 http.client._MAXHEADERS = 100000
 
-# url = "https://telemarket24.ru/catalog/"
+url = "https://telemarket24.ru/catalog/"
 headers = {
     "Accept": "*/*",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
 }
-# req = requests.get(url, headers=headers)
-# src = req.text
-#
-# with open ("index.html", "w", encoding="utf-8") as file:
-#    file.write(src)
+req = requests.get(url, headers=headers)
+src = req.text
+
+with open ("index.html", "w", encoding="utf-8") as file:
+   file.write(src)
 
 with open("index.html", encoding="utf-8") as file:
     src = file.read()
@@ -39,9 +39,9 @@ count = 0
 for category_name, category_href in all_categories.items():
     if count == 0:
         rep = [",", " ", "-"]
-        for item in rep:
-            if item in category_name:
-                category_name = category_name.replace(item, "_")
+        for char in rep:
+            if char in category_name:
+                category_name = category_name.replace(char, "_")
 
         req = requests.get(url=category_href, headers=headers)
         src = req.text
@@ -57,46 +57,53 @@ for category_name, category_href in all_categories.items():
         all_subcategories_data = soup.find(class_="catalog-menu-lvl1-wrap").find_all(class_="catalog-menu-lvl1")
         count_sub = 0
         for subcategory_data in all_subcategories_data:
-            if count_sub == 0:
-                subcategory_name = subcategory_data.find(class_="menu-lvl1-header").find(class_="menu-lvl1-link").find("span").text.strip()
-                subcategory_link = "https://telemarket24.ru" + subcategory_data.find(class_="menu-lvl1-header").find(class_="menu-lvl1-link").get("href")
+            #if count_sub == 0:
+            subcategory_name = subcategory_data.find(class_="menu-lvl1-header").find(class_="menu-lvl1-link").find("span").text.strip()
+            subcategory_link = "https://telemarket24.ru" + subcategory_data.find(class_="menu-lvl1-header").find(class_="menu-lvl1-link").get("href")
+            count_item = 0
+            if subcategory_data.ul != None:
                 subcategory_item_data = subcategory_data.ul.find_all("a")
-                len_item = len(subcategory_item_data)
-                count_item = 0
-                for item in subcategory_item_data:
-                    item_name = item.text.strip()
-                    item_link = "https://telemarket24.ru" + item.get("href")
+            else:
+                req = requests.get(url=subcategory_link, headers=headers)
+                src = req.text
+                soup = BeautifulSoup(src, "lxml")
+                all_subcategory_item_data = soup.find(class_="catalog blocks active")
+                subcategory_item_data = soup.find(class_="catalog blocks active").find_all("a", class_="link")
+            len_item = len(subcategory_item_data)
+            for item in subcategory_item_data:
+                item_name = item.text.strip()
+                item_link = "https://telemarket24.ru" + item.get("href")
 
-                    # req = requests.get(item_link, headers=headers)
-                    # src = req.text
-                    #
-                    # with open(f"data/{count}_{category_name}/{item_name}.html", "w", encoding="utf-8") as file:
-                    #     file.write(src)
+                req = requests.get(item_link, headers=headers)
+                src = req.text
 
-                    with open(f"data/{count}_{category_name}/{item_name}.html", encoding="utf-8") as file:
-                        src = file.read()
+                with open(f"data/{count}_{category_name}/{item_name}.html", "w", encoding="utf-8") as file:
+                    file.write(src)
 
-                    soup = BeautifulSoup(src, "lxml")
+                with open(f"data/{count}_{category_name}/{item_name}.html", encoding="utf-8") as file:
+                    src = file.read()
 
-                    product = soup.find(class_="main-data")
-                    product_name = product.find(class_="name").text.strip()
-                    product_availability = product.find(class_="info-tag").find("span").text.strip()
-                    product_price = product.find(class_="price").find(class_="value").text.strip()
+                soup = BeautifulSoup(src, "lxml")
 
-                    excel_sheet["A1"] = "Наименование товара"
-                    excel_sheet["B1"] = "Наличие товара, ед."
-                    excel_sheet["C1"] = "Стоимость товара,руб."
-                    excel_sheet["D1"] = "Ссылка на товар"
+                product = soup.find(class_="main-data") # if ... container style2022 product-page
+                product_name = product.find(class_="name").text.strip()
+                product_availability = product.find(class_="info-tag").find("span").text.strip()
+                product_price = product.find(class_="price").find(class_="value").text.strip()
 
-                    if count_item == 0:
-                        print(f"--- ЗАПОЛНЯЕМ ФАЙЛ {category_name}/{subcategory_name}--- \nЗаголовки добавлены.")
-                    excel_sheet.append([product_name, product_availability, product_price, item_link])
-                    print(f"     Добавлен товар: || {product_name}")
-                    excel_file.save(filename=f"data/{count}_{category_name}/{subcategory_name}.xlsx")
-                    if count_item + 1 == len_item:
-                        print(f"*** ФАЙЛ {category_name}/{subcategory_name} CОХРАНЕН ***")
+                excel_sheet["A1"] = "Наименование товара"
+                excel_sheet["B1"] = "Наличие товара, ед."
+                excel_sheet["C1"] = "Стоимость товара,руб."
+                excel_sheet["D1"] = "Ссылка на товар"
 
-                    count_item += 1
+                if count_item == 0:
+                    print(f"--- ЗАПОЛНЯЕМ ФАЙЛ {category_name}/{subcategory_name}--- \nЗаголовки добавлены.")
+                excel_sheet.append([product_name, product_availability, product_price, item_link])
+                print(f"     Добавлен товар: || {product_name}")
+                excel_file.save(filename=f"data/{count}_{category_name}/{subcategory_name}.xlsx")
+                if count_item + 1 == len_item:
+                    print(f"*** ФАЙЛ {category_name}/{subcategory_name} CОХРАНЕН ***")
 
-                count_sub += 1
+                count_item += 1
+
+            count_sub += 1
         count += 1
