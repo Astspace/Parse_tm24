52683698d7bb0cf71454105a5cd55911a40a3905
Astspace <asteroid.space@gmail.com>
2024-03-07 10:08:25 +0300

    working version

--- Parse.py
+++ Parse.py
@@ -7,16 +7,16 @@ import os
 
 http.client._MAXHEADERS = 100000
 
-url = "https://telemarket24.ru/catalog/"
+# url = "https://telemarket24.ru/catalog/"
 headers = {
     "Accept": "*/*",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
 }
-req = requests.get(url, headers=headers)
-src = req.text
+# req = requests.get(url, headers=headers)
+# src = req.text
 
-with open("index.html", "w", encoding="utf-8") as file:
-   file.write(src)
+# with open("index.html", "w", encoding="utf-8") as file:
+#    file.write(src)
 
 with open("index.html", encoding="utf-8") as file:
     src = file.read()
